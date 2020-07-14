eb4f3e12e26be14d4a791fda42ffa72881caf907
AVData <AVData@users.noreply.github.com>
2020-07-13 22:45:54 -0700

    quick test of the wrangle function, test failed due to nonexistent positional argument

--- wrangle_function.py
+++ wrangle_function.py
@@ -19,16 +19,15 @@ from IPython.display import IFrame
 
 
 def wrangle_function(dataframe):
-    '''
+    '''This function takes in the SleepCycle app csv file, creates and cleans a dataframe, and generates statistics given a the date specified.
     Args:
-        Dataframe
+        dataframe: the dataframe argument takes in/is the the CSV file from the SleepCycle app
+        split_date: The split_date argument is the date where the user would like to split the data frame.  This useful in looking at the statistical difference between two stretches of time, given a specific split date.
     Returns:
         Test-statistic
         P-Value
-        Distribution Plots
-        Line Plots
-        Bar Plots
     '''
+    split_date = 0
     df = pd.read_csv(f'./{dataframe}.csv', delimeter=';')
     df = df.drop(columns=['Heart rate', 'Wake up', 'Sleep Notes'])
     df['Sleep quality'] = df['Sleep quality'].str.rstring('%').astype('float')/100
@@ -78,61 +77,109 @@ def wrangle_function(dataframe):
         
         test_statistic, p_value = stats.ttest_ind(df['Sleep quality'][:401], df['Sleep quality'][401:], nan_policy='omit')
         
-        index_vals = df[df['Sleep quality'] < .2].index
-        df.drop(index_vals, inplace=True)
-        
-        # Distributions plots:
-        fig, ax_4 = plt.subplots()
-        ax_4 = sns.distplot(df['Time in bed'][:401], hist=False, color='r', label='Before')
-        ax_4 = sns.distplot(df['Time in bed'][401:], hist=False, label='During')
-        ax_4 = sns.distplot(df['Time in bed'], hist=False, color='black', label='Overall')
-        ax_4.legend()
+        return f"Test Statistic: {test_statistic}, \np-value: {p_value} \nDataFrame {df}"
+    
+def visualisations():
+    '''This function will create visualizations of the sleep data acquired from the SleepCycle app.
+    
+    '''
+    index_vals = df[df['Sleep quality'] < .2].index
+    df.drop(index_vals, inplace=True)
+    
+    # Subplots
+    # sns.set_style('white')
+    # Distributions plots:
+    fig, ax_4 = plt.subplots()
+    ax_4 = sns.distplot(
+                        df['Time in bed'][:401],
+                        hist=False, color='r',
+                        label='Before'
+                        )
+    ax_4 = sns.distplot(
+                        df['Time in bed'][401:],
+                        hist=False,
+                        label='During'
+                        )
+    ax_4 = sns.distplot(
+                        df['Time in bed'],
+                        hist=False,
+                        color='black',
+                        label='Overall'
+                        )
+    ax_4.legend()
 
-        fig, ax = plt.subplots()
-        ax = sns.distplot(df['Sleep quality'][:401], hist=False, color='r', label='Before')
-        ax = sns.distplot(df['Sleep quality'][401:], hist=False, label='During')
-        ax = sns.distplot(df['Sleep quality'], hist=False, color='black', label='Overall')
-        ax.legend()
-        
-        # Lineplots       
-        fig_1, ax_1 = plt.subplots()
-        ax_1 = sns.lineplot(y=df['Sleep quality'], x=df['End'].dt.hour, hue=df['Before and During'], err_style='bars')
-        
-        # Bar plots
-        fig_2, ax_2 = plt.suplots()
-        ax_2 = sns.barplot(
-                   x='Weekday to bed (name)',
-                   y=df['Time in bed'],
-                   data=df,
-                   hue='Before and During',
-                   order=[
-                       'Sunday',
-                       'Monday',
-                       'Tuesday',
-                       'Wednesday',
-                       'Thursday',
-                       'Friday',
-                       'Saturday'
-                           ],
-                   color='salmon'
-                    )
-         ax_2.legend()
-        
-        fig_3, ax_3 = plt.subplots()
-        ax_3 = sns.barplot(
-                   x='Weekday to bed (name)',
-                   y=df['Sleep quality'],
-                   data=df,
-                   hue='Before and During',
-                   order=[
-                       'Sunday',
-                       'Monday',
-                       'Tuesday',
-                       'Wednesday',
-                       'Thursday',
-                       'Friday',
-                       'Saturday'
-                           ],
-                   color='salmon',
-                    )
-        ax_3.legend()
+    fig, ax = plt.subplots()
+    ax = sns.distplot(
+                        df['Sleep quality'][:401],
+                        hist=False,
+                        color='r',
+                        label='Before'
+                        )
+    ax = sns.distplot(
+                        df['Sleep quality'][401:],
+                        hist=False,
+                        label='During'
+                        )
+    ax = sns.distplot(
+                        df['Sleep quality'],
+                        hist=False,
+                        color='black',
+                        label='Overall'
+                        )
+    ax.legend()
+
+
+    # Line plots
+    fig_1, ax_1 = plt.subplots()
+    ax_1 = sns.lineplot(
+                        y=df['Sleep quality'],
+                        x=df['End'].dt.hour,
+                        hue=df['Before and During'],
+                        err_style='bars'
+                        )
+
+
+    # Box plots
+    fig_2, ax_2 = plt.subplots()
+    ax_2 = sns.barplot(
+                       x='Weekday to bed (name)',
+                       y=df['Time in bed'],
+                       data=df,
+                       hue='Before and During',
+                       order=[
+                           'Sunday',
+                           'Monday',
+                           'Tuesday',
+                           'Wednesday',
+                           'Thursday',
+                           'Friday',
+                           'Saturday'
+                               ],
+                       color='salmon'
+                        )
+    ax_2.legend()
+
+
+    # Barplots
+    fig_3, ax_3 = plt.subplots()
+    ax_3 = sns.barplot(
+                       x='Weekday to bed (name)',
+                       y=df['Sleep quality'],
+                       data=df,
+                       hue='Before and During',
+                       order=[
+                           'Sunday',
+                           'Monday',
+                           'Tuesday',
+                           'Wednesday',
+                           'Thursday',
+                           'Friday',
+                           'Saturday'
+                               ],
+                       color='salmon',
+                        )
+    ax_3.legend()
+    sns.despine()
+    ax_3.set_title('Day to bed')
+
+    return f"{ax_4.legend()} \n{ax.legend()} \n{ax_1} \n{ax_2.legend()} \n{ax_3.set_title('Day to bed')}"
